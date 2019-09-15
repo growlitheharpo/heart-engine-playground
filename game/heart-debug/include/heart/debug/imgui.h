fcb3ee4a2dca47fe534240df24889d0e0a760da3
James Keats <growlitheharpo@gmail.com>
2019-09-15 19:39:34 -0400

    Add imgui

---
+++ game/heart-debug/include/heart/debug/imgui.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <heart/config.h>
+
+#if !defined(IMGUI_ENABLED)
+#if HEART_STRICT_PERF
+#define IMGUI_ENABLED 0
+#else
+#define IMGUI_ENABLED 1
+#endif
+#endif
+
+#if IMGUI_ENABLED
+
+#include <imgui.h>
+#include <imgui-SFML.h>
+
+#endif
