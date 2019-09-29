cf4d132b1b2d681b40bdd685c39ff128d4f3b5a4
James Keats <growlitheharpo@gmail.com>
2019-09-29 08:44:12 -0400

    Add a utility header for directly parsing from a filename

---
+++ game/heart-core/include/heart/deserialization_file.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <heart/deserialization.h>
+#include <heart/file.h>
+
+#include <heart/debug/assert.h>
+
+template <typename OutType>
+bool DeserializeObjectFromFile(OutType& outObject, const char* filename)
+{
+	// 4 kilobytes should be enough for just json... right?
+	uint8_t filebuffer[4096] = {};
+
+	HeartFile file;
+	uint64_t size;
+	if (!HeartOpenFile(file, filename, HeartOpenFileMode::ReadExisting))
+		return false;
+
+	if (!HeartGetFileSize(file, size))
+		return false;
+
+	if (!HEART_CHECK(size < sizeof(filebuffer), "Bump buffer size in DeserializeObjectFromFile or use dynamic alloc!"))
+		return false;
+
+	if (!HeartReadFile(file, filebuffer, size))
+		return false;
+
+	rapidjson::Document jsonDoc;
+	jsonDoc.Parse((char*)(filebuffer));
+
+	if (jsonDoc.HasParseError())
+		return false;
+
+	return DeserializeObject(outObject, jsonDoc);
+}
