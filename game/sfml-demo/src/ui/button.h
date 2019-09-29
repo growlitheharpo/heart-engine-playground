39950ce3f2a29c86d98ddd04c7c374474f483e11
James Keats <growlitheharpo@gmail.com>
2019-09-28 21:12:13 -0400

    Continue to stub out UI system

--- game/sfml-demo/src/ui/button.h
+++ game/sfml-demo/src/ui/button.h
@@ -1,6 +1,23 @@
 #pragma once
 
+#include "ui/widget.h"
+#include "events/events.h"
+
 namespace UI
 {
+	class Button : public Widget
+	{
+	private:
+		EventManager::EventFilterFunc event_handle_;
 
+	public:
+		~Button();
+
+		// Inherited via Widget
+		virtual void Initialize() const override;
+
+		virtual void Update() const override;
+
+		virtual void Draw() const override;
+	};
 }
