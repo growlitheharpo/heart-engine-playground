fcc075b27c1a1450564e9d59d18c2cac4c594be6
James Keats <growlitheharpo@gmail.com>
2019-10-05 21:17:42 -0400

    Cleanup heart deserialization and fix naming

--- game/heart-core/include/heart/deserialization.h
+++ game/heart-core/include/heart/deserialization.h
@@ -12,21 +12,14 @@
 
 #include <rapidjson/document.h>
 
-template <typename T>
-struct is_entt_meta_any : hrt::false_type
+namespace heart_priv
 {
-};
-
-template <>
-struct is_entt_meta_any<entt::meta_any> : hrt::true_type
-{
-};
-
-template <typename OutType, typename MetaType, typename RapidjsonType>
-static bool ReadSingleProperty(OutType& outObject, MetaType& metaData, RapidjsonType& jsonNode);
+	template <typename OutType, typename MetaType, typename RapidjsonType>
+	static bool ReadSingleProperty(OutType& outObject, MetaType& metaData, RapidjsonType& jsonNode);
+}
 
 template <typename OutType, typename RapidjsonType>
-bool DeserializeObject(OutType& outObject, RapidjsonType& node)
+bool HeartDeserializeObject(OutType& outObject, RapidjsonType& node)
 {
 	if (!HEART_CHECK(node.IsObject()))
 		return false;
@@ -41,7 +34,7 @@ bool DeserializeObject(OutType& outObject, RapidjsonType& node)
 	if (!HEART_CHECK(metaType, "Type is not reflected!", typeStr))
 		return false;
 
-	if constexpr (is_entt_meta_any<hrt::remove_reference_t<decltype(outObject)>>::value)
+	if constexpr (hrt::is_same<entt::meta_any, hrt::remove_reference_t<decltype(outObject)>>::value)
 	{
 		if (!HEART_CHECK(outObject.type() == metaType, "DeserializeObject was passed meta_handle of the wrong type!"))
 			return false;
@@ -57,91 +50,95 @@ bool DeserializeObject(OutType& outObject, RapidjsonType& node)
 		HEART_CHECK(child.name.IsString());
 		auto childId = entt::hashed_string::to_value(child.name.GetString());
 
-		if (!ReadSingleProperty(outObject, metaType.data(childId), child.value))
+		if (!heart_priv::ReadSingleProperty(outObject, metaType.data(childId), child.value))
 			return false;
 	}
 
 	return true;
 }
 
-template <typename OutType, typename MetaType, typename RapidjsonType>
-static bool ReadSingleProperty(OutType& outObject, MetaType& metaData, RapidjsonType& jsonNode, size_t index)
-{
-	if (jsonNode.IsObject())
-	{
-		auto& instance = metaData.get(outObject, index);
-		if (!DeserializeObject(instance, jsonNode))
-			return false;
-	}
-	else if (jsonNode.IsString())
-	{
-		metaData.set(outObject, index, jsonNode.GetString());
-	}
-	else if (jsonNode.IsBool())
-	{
-		metaData.set(outObject, index, jsonNode.GetBool());
-	}
-	else if (jsonNode.IsInt())
-	{
-		metaData.set(outObject, index, jsonNode.GetInt());
-	}
-	else if (jsonNode.IsUint())
-	{
-		metaData.set(outObject, index, jsonNode.GetUint());
-	}
-	else if (jsonNode.IsFloat())
-	{
-		metaData.set(outObject, index, jsonNode.GetFloat());
-	}
-	else if (jsonNode.IsArray())
-	{
-		HEART_ASSERT(false, "Heart Deserialization cannot parse 2D arrays!");
-		return false;
-	}
 
-	return true;
-}
-
-template <typename OutType, typename MetaType, typename RapidjsonType>
-static bool ReadSingleProperty(OutType& outObject, MetaType& metaData, RapidjsonType& jsonNode)
+namespace heart_priv
 {
-	if (jsonNode.IsObject())
+	template <typename OutType, typename MetaType, typename RapidjsonType>
+	static bool ReadSingleProperty(OutType& outObject, MetaType& metaData, RapidjsonType& jsonNode, size_t index)
 	{
-		auto& instance = metaData.get(outObject);
-		if (!DeserializeObject(instance, jsonNode))
-			return false;
-	}
-	else if (jsonNode.IsString())
-	{
-		metaData.set(outObject, jsonNode.GetString());
-	}
-	else if (jsonNode.IsBool())
-	{
-		metaData.set(outObject, jsonNode.GetBool());
-	}
-	else if (jsonNode.IsInt())
-	{
-		metaData.set(outObject, jsonNode.GetInt());
-	}
-	else if (jsonNode.IsUint())
-	{
-		metaData.set(outObject, jsonNode.GetUint());
-	}
-	else if (jsonNode.IsFloat())
-	{
-		metaData.set(outObject, jsonNode.GetFloat());
-	}
-	else if (jsonNode.IsArray())
-	{
-		auto jsonArr = jsonNode.GetArray();
-		for (rapidjson::SizeType i = 0; i < jsonArr.Size(); ++i)
+		if (jsonNode.IsObject())
 		{
-			if (!ReadSingleProperty(outObject, metaData, jsonArr[i], size_t(i)))
+			auto& instance = metaData.get(outObject, index);
+			if (!HeartDeserializeObject(instance, jsonNode))
 				return false;
 		}
+		else if (jsonNode.IsString())
+		{
+			metaData.set(outObject, index, jsonNode.GetString());
+		}
+		else if (jsonNode.IsBool())
+		{
+			metaData.set(outObject, index, jsonNode.GetBool());
+		}
+		else if (jsonNode.IsInt())
+		{
+			metaData.set(outObject, index, jsonNode.GetInt());
+		}
+		else if (jsonNode.IsUint())
+		{
+			metaData.set(outObject, index, jsonNode.GetUint());
+		}
+		else if (jsonNode.IsFloat())
+		{
+			metaData.set(outObject, index, jsonNode.GetFloat());
+		}
+		else if (jsonNode.IsArray())
+		{
+			HEART_ASSERT(false, "Heart Deserialization cannot parse 2D arrays!");
+			return false;
+		}
+
+		return true;
 	}
 
-	return true;
+	template <typename OutType, typename MetaType, typename RapidjsonType>
+	static bool ReadSingleProperty(OutType& outObject, MetaType& metaData, RapidjsonType& jsonNode)
+	{
+		if (jsonNode.IsObject())
+		{
+			auto& instance = metaData.get(outObject);
+			if (!HeartDeserializeObject(instance, jsonNode))
+				return false;
+		}
+		else if (jsonNode.IsString())
+		{
+			metaData.set(outObject, jsonNode.GetString());
+		}
+		else if (jsonNode.IsBool())
+		{
+			metaData.set(outObject, jsonNode.GetBool());
+		}
+		else if (jsonNode.IsInt())
+		{
+			metaData.set(outObject, jsonNode.GetInt());
+		}
+		else if (jsonNode.IsUint())
+		{
+			metaData.set(outObject, jsonNode.GetUint());
+		}
+		else if (jsonNode.IsFloat())
+		{
+			metaData.set(outObject, jsonNode.GetFloat());
+		}
+		else if (jsonNode.IsArray())
+		{
+			auto jsonArr = jsonNode.GetArray();
+			for (rapidjson::SizeType i = 0; i < jsonArr.Size(); ++i)
+			{
+				if (!ReadSingleProperty(outObject, metaData, jsonArr[i], size_t(i)))
+					return false;
+			}
+		}
+
+		return true;
+	}
 }
 
 #define BEGIN_SERIALIZE_TYPE(type_name) entt::reflect<type_name>(#type_name##_hs)
