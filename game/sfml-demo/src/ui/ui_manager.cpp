ade7d85fabe8577b71f6d550e04b494f17ac7043
James Keats <growlitheharpo@gmail.com>
2019-10-05 21:22:57 -0400

    Add serialized global button functionality and hook it into buttons

--- game/sfml-demo/src/ui/ui_manager.cpp
+++ game/sfml-demo/src/ui/ui_manager.cpp
@@ -1,2 +1,22 @@
 #include "ui_manager.h"
 
+#include "events/events.h"
+
+bool UI::GlobalButtonFunctionality::CloseGame()
+{
+	sf::Event e;
+	e.type = sf::Event::Closed;
+
+	EventManager::Get().ManuallyIssueEvent(e);
+	return true;
+}
+
+bool UI::GlobalButtonFunctionality::TogglePause()
+{
+	return true;
+}
+
+bool UI::GlobalButtonFunctionality::ResetPlayer()
+{
+	return true;
+}
--- game/sfml-demo/src/ui/ui_manager.h
+++ game/sfml-demo/src/ui/ui_manager.h
@@ -1,6 +1,24 @@
 #pragma once
 
+#include <heart/deserialization_fwd.h>
+
 namespace UI
 {
+	SERIALIZE_STRUCT()
+	struct GlobalButtonFunctionality
+	{
+		static GlobalButtonFunctionality Instance()
+		{
+			return GlobalButtonFunctionality();
+		}
 
+		SERIALIZE_MEMBER_METHOD() static bool CloseGame();
+		SERIALIZE_MEMBER_METHOD() static bool TogglePause();
+		SERIALIZE_MEMBER_METHOD() static bool ResetPlayer();
+	};
+
+	class UIManager
+	{
+	public:
+	};
 }
