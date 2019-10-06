e1f455c02c1567dadf1c638e103db6010d15fab9
James Keats <growlitheharpo@gmail.com>
2019-10-05 21:21:08 -0400

    Start setting up a skeleton for UI buttons

--- game/sfml-demo/src/ui/button.h
+++ game/sfml-demo/src/ui/button.h
@@ -1,22 +1,40 @@
 #pragma once
 
-#include "ui/widget.h"
 #include "events/events.h"
+#include "ui/widget.h"
+
+#include <heart/deserialization_fwd.h>
 
 namespace UI
 {
 	class Button : public Widget
 	{
+	public:
+		SERIALIZE_STRUCT()
+		struct ButtonData
+		{
+			float offsetX, offsetY;
+			float sizeX, sizeY;
+			int offsetSizeMode;
+			SerializedString<32> label;
+			SerializedString<48> action;
+		};
+
 	private:
-		EventManager::EventFilterFunc event_handle_;
+		EventManager::EventFilterFunc click_event_handle_;
+		ButtonData data_;
+
+		bool OnClick(sf::Event e);
 
 	public:
 		~Button();
 
 		// Inherited via Widget
-		virtual void Initialize() const override;
+		virtual void Initialize() override;
 
-		virtual void Update() const override;
+		virtual void Destroy() override;
+
+		virtual void Update() override;
 
 		virtual void Draw() const override;
 	};
