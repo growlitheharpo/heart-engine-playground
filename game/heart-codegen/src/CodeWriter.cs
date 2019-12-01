10d48060c60c714b69f66cee7ba2ee6ec91cbe88
James Keats <growlitheharpo@gmail.com>
2019-11-30 20:14:50 -0500

    Refactor code gen to be slightly cleaner

---
+++ game/heart-codegen/src/CodeWriter.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Heart.Codegen
+{
+	public interface ICodeWriter
+	{
+		void Dispose();
+
+		void WriteLine(string line);
+		void WriteLine();
+
+		ICodeWriter GetParent();
+		int GetIndentLevel();
+	}
+
+	public abstract class BaseCodeWriter : ICodeWriter, IDisposable
+	{
+		private ICodeWriter _parent;
+
+		protected List<string> _lines = new List<string>();
+		protected int _indentLevel = 0;
+
+		protected BaseCodeWriter(ICodeWriter parent)
+		{
+			_parent = parent;
+		}
+
+		public void WriteLine(string line)
+		{
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < _indentLevel; ++i)
+				sb.Append("\t");
+			sb.Append(line);
+			_lines.Add(sb.ToString());
+		}
+
+		public void WriteLine()
+		{
+			_lines.Add(string.Empty);
+		}
+
+		public ICodeWriter GetParent()
+		{
+			return _parent;
+		}
+
+		public int GetIndentLevel()
+		{
+			return _indentLevel;
+		}
+
+		public abstract void Dispose();
+	}
+
+	public class CodeWriter : BaseCodeWriter
+	{
+		StreamWriter _stream;
+
+		public CodeWriter(StreamWriter stream) : base(null)
+		{
+			_stream = stream;
+		}
+
+		public override void Dispose()
+		{
+			foreach (var l in _lines)
+				_stream.WriteLine(l);
+		}
+	}
+
+	public class IndentLevel : BaseCodeWriter
+	{
+		private string _endLine;
+
+		public IndentLevel(ICodeWriter parent, string firstLine = "", string endLine = "") : base(parent)
+		{
+			if (!string.IsNullOrEmpty(firstLine))
+				WriteLine(firstLine);
+
+			_endLine = endLine;
+
+			++_indentLevel;
+		}
+
+		public override void Dispose()
+		{
+			--_indentLevel;
+			if (!string.IsNullOrEmpty(_endLine))
+			{
+				WriteLine(_endLine);
+			}
+
+			foreach (var l in _lines)
+				GetParent().WriteLine(l);
+		}
+	}
+
+	public class FunctionBlock : BaseCodeWriter
+	{
+		public FunctionBlock(ICodeWriter parent, string functionProto) : base(parent)
+		{
+			WriteLine(functionProto);
+			WriteLine("{");
+			++_indentLevel;
+		}
+
+		public override void Dispose()
+		{
+			--_indentLevel;
+			WriteLine("}");
+
+			foreach (var l in _lines)
+				GetParent().WriteLine(l);
+		}
+	}
+}
