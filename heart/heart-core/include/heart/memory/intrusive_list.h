063dca4d71e196e37ccf70a4f08d18c925c76116
James Keats <growlitheharpo@users.noreply.github.com>
2022-04-07 17:27:30 -0400

    Add intrusive list (#23)
    * Add intrusive list
    
    * Much more fleshed out list

---
+++ heart/heart-core/include/heart/memory/intrusive_list.h
@@ -0,0 +1,362 @@
+#pragma once
+
+#include <heart/config.h>
+#include <heart/copy_move_semantics.h>
+#include <heart/types.h>
+
+#include <heart/util/canonical_typedefs.h>
+
+#include <type_traits>
+
+struct HeartIntrusiveListLink
+{
+	HeartIntrusiveListLink* prev = nullptr;
+	HeartIntrusiveListLink* next = nullptr;
+};
+
+template <typename T>
+struct HeartIntrusiveListIterator;
+
+template <typename T>
+struct HeartIntrusiveListConstIterator;
+
+template <typename T, HeartIntrusiveListLink T::*LinkPointer>
+class HeartIntrusiveList
+{
+public:
+	DECLARE_STANDARD_TYPEDEFS(T);
+	using iterator = HeartIntrusiveListIterator<HeartIntrusiveList<T, LinkPointer>>;
+	using const_iterator = HeartIntrusiveListConstIterator<HeartIntrusiveList<T, LinkPointer>>;
+	using link_type = HeartIntrusiveListLink;
+	typedef link_type value_type::*link_pointer_t;
+
+	friend struct iterator;
+	friend struct const_iterator;
+
+	static_assert(std::is_standard_layout<value_type>::value);
+
+public:
+	HeartIntrusiveList()
+	{
+		m_root.next = &m_root;
+		m_root.prev = &m_root;
+		m_size = 0;
+	}
+
+	DISABLE_COPY_SEMANTICS(HeartIntrusiveList);
+
+	HeartIntrusiveList(HeartIntrusiveList&& o) :
+		HeartIntrusiveList()
+	{
+		AddLinkBetweenLinks(&m_root, &o.m_root, o.m_root.next);
+		RemoveLink(&o.m_root);
+		m_size = o.m_size;
+
+		o.m_root.next = &o.m_root;
+		o.m_root.prev = &o.m_root;
+		o.m_size = 0;
+	}
+
+	HeartIntrusiveList& operator=(HeartIntrusiveList&& o)
+	{
+		// Should this be an error if we're not empty?
+		Clear();
+
+		AddLinkBetweenLinks(&m_root, &o.m_root, o.m_root.next);
+		RemoveLink(&o.m_root);
+		m_size = o.m_size;
+
+		o.m_root.next = &o.m_root;
+		o.m_root.prev = &o.m_root;
+		o.m_size = 0;
+
+		return *this;
+	}
+
+	void PushFront(pointer p)
+	{
+		AddLinkBetweenLinks(LinkFromPointer(p), &m_root, m_root.next);
+	}
+
+	void PushBack(pointer p)
+	{
+		AddLinkBetweenLinks(LinkFromPointer(p), m_root.prev, &m_root);
+	}
+
+	pointer PopFront()
+	{
+		if (IsEmpty())
+			return nullptr;
+
+		pointer element = PointerFromLink(m_root.next);
+		Remove(element);
+		return element;
+	}
+
+	pointer PopBack()
+	{
+		if (IsEmpty())
+			return nullptr;
+
+		pointer element = PointerFromLink(m_root.prev);
+		Remove(element);
+		return element;
+	}
+
+	POINTER_AND_CONST_POINTER(Front(),
+		{
+			if (IsEmpty())
+				return nullptr;
+
+			return PointerFromLink(m_root.next);
+		})
+
+	POINTER_AND_CONST_POINTER(Back(),
+		{
+			if (IsEmpty())
+				return nullptr;
+
+			return PointerFromLink(m_root.prev);
+		})
+
+	void Remove(pointer p)
+	{
+		RemoveLink(LinkFromPointer(p));
+	}
+
+	void Remove(iterator iterator)
+	{
+		RemoveLink(iterator.link);
+	}
+
+	void Clear()
+	{
+		while (!IsEmpty())
+		{
+			RemoveLink(m_root.prev);
+		}
+	}
+
+	bool Contains(pointer p) const
+	{
+		const link_type* link = m_root.next;
+		while (link != &m_root)
+		{
+			if (PointerFromLink(link) == p)
+				return true;
+
+			link = link->next;
+		}
+
+		return false;
+	}
+
+	size_t Size() const
+	{
+		return m_size;
+	}
+
+	bool IsEmpty() const
+	{
+		return Size() == 0;
+	}
+
+	iterator begin()
+	{
+		return iterator(*this, m_root.next);
+	}
+
+	iterator end()
+	{
+		return iterator(*this, &m_root);
+	}
+
+	const_iterator begin() const
+	{
+		return const_iterator(*this, m_root.next);
+	}
+
+	const_iterator end() const
+	{
+		return const_iterator(*this, &m_root);
+	}
+
+private:
+	static link_type* LinkFromPointer(pointer p)
+	{
+		return &(p->*LinkPointer);
+	}
+
+	pointer PointerFromLink(link_type* link) const
+	{
+		if (link == &m_root)
+			return nullptr;
+
+		return reinterpret_cast<pointer>(reinterpret_cast<char*>(link) - reinterpret_cast<ptrdiff_t>(&(reinterpret_cast<value_type const*>(NULL)->*LinkPointer)));
+	}
+
+	const_pointer PointerFromLink(const link_type* link) const
+	{
+		if (link == &m_root)
+			return nullptr;
+
+		return reinterpret_cast<const_pointer>(reinterpret_cast<const char*>(link) - reinterpret_cast<ptrdiff_t>(&(reinterpret_cast<value_type const*>(NULL)->*LinkPointer)));
+	}
+
+	void AddLinkBetweenLinks(link_type* item, link_type* prev, link_type* next)
+	{
+		item->next = next;
+		item->prev = prev;
+		next->prev = item;
+		prev->next = item;
+		++m_size;
+	}
+
+	void RemoveLink(link_type* item)
+	{
+		if (item == &m_root)
+			return;
+
+		item->next->prev = item->prev;
+		item->prev->next = item->next;
+		item->next = nullptr;
+		item->prev = nullptr;
+		--m_size;
+	}
+
+private:
+	link_type m_root;
+	size_t m_size;
+};
+
+template <typename ListType>
+struct HeartIntrusiveListIterator
+{
+	using list_type = ListType;
+	using link_type = list_type::link_type;
+	using this_type = HeartIntrusiveListIterator<ListType>;
+	USING_STANDARD_TYPEDEFS(list_type);
+
+	list_type& list;
+	link_type* link;
+
+	HeartIntrusiveListIterator(list_type& l, link_type* p) :
+		list(l),
+		link(p)
+	{
+	}
+
+	REF_AND_CONST_REF(operator*(),
+		{
+			return *list.PointerFromLink(link);
+		})
+
+	POINTER_AND_CONST_POINTER(operator->(),
+		{
+			return list.PointerFromLink(link);
+		})
+
+	friend bool operator==(const this_type& lhs, const this_type& rhs)
+	{
+		return lhs.link == rhs.link;
+	}
+
+	bool operator<(const this_type& rhs) const
+	{
+		const auto& lhs = *this;
+
+		const link_type& root = lhs.list.m_root;
+		const link_type* link = root.next;
+		while (link != &root)
+		{
+			if (link == lhs.link)
+				return true;
+
+			if (link == rhs.link)
+				return false;
+
+			link = link->next;
+		}
+
+		return false;
+	}
+
+	this_type& operator++()
+	{
+		link = link->next;
+		return *this;
+	}
+
+	this_type operator++(int)
+	{
+		this_type tmp(list, link);
+		this->operator++();
+		return tmp;
+	}
+};
+
+template <typename ListType>
+struct HeartIntrusiveListConstIterator
+{
+	using list_type = ListType;
+	using link_type = list_type::link_type;
+	using this_type = HeartIntrusiveListConstIterator<ListType>;
+	USING_STANDARD_TYPEDEFS(list_type);
+
+	const list_type& list;
+	const link_type* link;
+
+	HeartIntrusiveListConstIterator(const list_type& l, const link_type* p) :
+		list(l),
+		link(p)
+	{
+	}
+
+	const_reference operator*() const
+	{
+		return *list.PointerFromLink(link);
+	}
+
+	const_pointer operator->() const
+	{
+		return list.PointerFromLink(link);
+	}
+
+	friend bool operator==(const this_type& lhs, const this_type& rhs)
+	{
+		return lhs.link == rhs.link;
+	}
+
+	bool operator<(const this_type& rhs) const
+	{
+		auto& lhs = *this;
+
+		const link_type& root = lhs.list.m_root;
+		const link_type* link = root.next;
+		while (link != &root)
+		{
+			if (link == lhs.link)
+				return true;
+
+			if (link == rhs.link)
+				return false;
+
+			link = link->next;
+		}
+
+		return false;
+	}
+
+	this_type& operator++()
+	{
+		link = link->next;
+		return *this;
+	}
+
+	this_type operator++(int)
+	{
+		this_type tmp(list, link);
+		this->operator++();
+		return tmp;
+	}
+};
---
+++ heart/heart-test/src/intrusive_list.cpp
@@ -0,0 +1,433 @@
+#include <heart/memory/intrusive_list.h>
+
+#include <gtest/gtest.h>
+
+struct SimpleListEntryType
+{
+	char label = 0;
+	HeartIntrusiveListLink link;
+};
+
+using SimpleHeartIntrusiveList = HeartIntrusiveList<SimpleListEntryType, &SimpleListEntryType::link>;
+
+TEST(IntrusiveList, EmptyListIterators)
+{
+	SimpleHeartIntrusiveList list;
+
+	EXPECT_TRUE(list.begin() == list.end());
+}
+
+TEST(IntrusiveList, EmptyListSize)
+{
+	SimpleHeartIntrusiveList list;
+
+	EXPECT_EQ(list.Size(), 0);
+	EXPECT_TRUE(list.IsEmpty());
+}
+
+TEST(IntrusiveList, PushBack)
+{
+	SimpleListEntryType a {'a'};
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+
+	EXPECT_EQ(list.Size(), 1);
+}
+
+TEST(IntrusiveList, PushFront)
+{
+	SimpleListEntryType a {'a'};
+
+	SimpleHeartIntrusiveList list;
+	list.PushFront(&a);
+
+	EXPECT_EQ(list.Size(), 1);
+}
+
+TEST(IntrusiveList, Contains)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+
+	EXPECT_EQ(list.Size(), 1);
+
+	EXPECT_TRUE(list.Contains(&a));
+	EXPECT_FALSE(list.Contains(&b));
+}
+
+TEST(IntrusiveList, Front)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+	list.PushBack(&b);
+
+	SimpleListEntryType* front = list.Front();
+	EXPECT_EQ(front, &a);
+
+	const SimpleHeartIntrusiveList& listRef = list;
+	const SimpleListEntryType* constFront = listRef.Front();
+	EXPECT_EQ(constFront, &a);
+}
+
+TEST(IntrusiveList, Back)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+	list.PushBack(&b);
+
+	SimpleListEntryType* front = list.Back();
+	EXPECT_EQ(front, &b);
+
+	const SimpleHeartIntrusiveList& listRef = list;
+	const SimpleListEntryType* constFront = listRef.Back();
+	EXPECT_EQ(constFront, &b);
+}
+
+TEST(IntrusiveList, PopBack)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+	list.PushBack(&b);
+
+	EXPECT_EQ(list.Size(), 2);
+	EXPECT_TRUE(list.Contains(&a));
+	EXPECT_TRUE(list.Contains(&b));
+
+	SimpleListEntryType* entry = list.PopBack();
+	EXPECT_EQ(entry, &b);
+	EXPECT_FALSE(list.Contains(&b));
+	EXPECT_TRUE(list.Contains(&a));
+}
+
+TEST(IntrusiveList, PopFront)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+	list.PushBack(&b);
+
+	EXPECT_EQ(list.Size(), 2);
+	EXPECT_TRUE(list.Contains(&a));
+	EXPECT_TRUE(list.Contains(&b));
+
+	SimpleListEntryType* entry = list.PopFront();
+	EXPECT_EQ(entry, &a);
+	EXPECT_FALSE(list.Contains(&a));
+	EXPECT_TRUE(list.Contains(&b));
+}
+
+TEST(IntrusiveList, RemovePointer)
+{
+	SimpleListEntryType a {'a'};
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+
+	EXPECT_TRUE(list.Contains(&a));
+
+	list.Remove(&a);
+
+	EXPECT_FALSE(list.Contains(&a));
+}
+
+TEST(IntrusiveList, RemoveIterator)
+{
+	SimpleListEntryType a {'a'};
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+
+	EXPECT_TRUE(list.Contains(&a));
+
+	auto iter = list.begin();
+	list.Remove(iter);
+
+	EXPECT_FALSE(list.Contains(&a));
+}
+
+TEST(IntrusiveList, Clear)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+	SimpleListEntryType c {'c'};
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+	list.PushBack(&b);
+	list.PushBack(&c);
+
+	EXPECT_EQ(list.Size(), 3);
+	list.Clear();
+	EXPECT_EQ(list.Size(), 0);
+	EXPECT_FALSE(list.Contains(&a));
+	EXPECT_FALSE(list.Contains(&b));
+	EXPECT_FALSE(list.Contains(&c));
+}
+
+TEST(IntrusiveList, BasicIteration)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+	SimpleListEntryType c {'c'};
+
+	int count = 0;
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+	list.PushBack(&b);
+	list.PushBack(&c);
+
+	for (SimpleListEntryType& entry : list)
+	{
+		if (count == 0)
+		{
+			EXPECT_EQ(&entry, &a);
+		}
+		else if (count == 1)
+		{
+			EXPECT_EQ(&entry, &b);
+		}
+		else if (count == 2)
+		{
+			EXPECT_EQ(&entry, &c);
+		}
+
+		++count;
+	}
+
+	EXPECT_EQ(count, 3);
+}
+
+TEST(IntrusiveList, IteratorComparison)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+	SimpleListEntryType c {'c'};
+
+	int count = 0;
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+	list.PushBack(&b);
+	list.PushBack(&c);
+
+	auto iteratorA = list.begin();
+
+	auto iteratorB = list.begin();
+	++iteratorB;
+
+	auto iteratorC = list.begin();
+	++iteratorC;
+	++iteratorC;
+
+	auto iteratorD = list.begin();
+	++iteratorD;
+	++iteratorD;
+	++iteratorD;
+
+	EXPECT_LT(iteratorA, iteratorB);
+	EXPECT_LT(iteratorA, iteratorC);
+	EXPECT_LT(iteratorB, iteratorC);
+
+	EXPECT_LT(iteratorA, list.end());
+	EXPECT_LT(iteratorB, list.end());
+	EXPECT_LT(iteratorC, list.end());
+	EXPECT_EQ(iteratorD, list.end());
+}
+
+TEST(IntrusiveList, AddBothEnds)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+	SimpleListEntryType c {'c'};
+
+	SimpleHeartIntrusiveList list;
+
+	// Add b first
+	list.PushFront(&b);
+
+	// Add a in front of b and c after b
+	list.PushFront(&a);
+	list.PushBack(&c);
+
+	int count = 0;
+	for (SimpleListEntryType& entry : list)
+	{
+		if (count == 0)
+		{
+			EXPECT_EQ(&entry, &a);
+		}
+		else if (count == 1)
+		{
+			EXPECT_EQ(&entry, &b);
+		}
+		else if (count == 2)
+		{
+			EXPECT_EQ(&entry, &c);
+		}
+
+		++count;
+	}
+}
+
+TEST(IntrusiveList, Removal)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+	SimpleListEntryType c {'c'};
+
+	SimpleHeartIntrusiveList list;
+	list.PushBack(&a);
+	list.PushBack(&b);
+	list.PushBack(&c);
+
+	EXPECT_EQ(list.Size(), 3);
+
+	list.Remove(&b);
+
+	EXPECT_EQ(list.Size(), 2);
+
+	int count = 0;
+	for (SimpleListEntryType& entry : list)
+	{
+		if (count == 0)
+		{
+			EXPECT_EQ(&entry, &a);
+		}
+		else if (count == 1)
+		{
+			EXPECT_EQ(&entry, &c);
+		}
+
+		++count;
+	}
+
+	EXPECT_EQ(count, 2);
+}
+
+TEST(IntrusiveList, MoveConstructor)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+	SimpleListEntryType c {'c'};
+
+	SimpleHeartIntrusiveList listA;
+	listA.PushBack(&a);
+	listA.PushBack(&b);
+	listA.PushBack(&c);
+
+	EXPECT_EQ(listA.Size(), 3);
+
+	SimpleHeartIntrusiveList listB(std::move(listA));
+
+	EXPECT_EQ(listA.Size(), 0);
+	EXPECT_EQ(listB.Size(), 3);
+	EXPECT_FALSE(listA.Contains(&a));
+	EXPECT_FALSE(listA.Contains(&b));
+	EXPECT_FALSE(listA.Contains(&c));
+	EXPECT_TRUE(listB.Contains(&a));
+	EXPECT_TRUE(listB.Contains(&b));
+	EXPECT_TRUE(listB.Contains(&c));
+
+	int count = 0;
+	for (SimpleListEntryType& entry : listB)
+	{
+		if (count == 0)
+		{
+			EXPECT_EQ(&entry, &a);
+		}
+		else if (count == 1)
+		{
+			EXPECT_EQ(&entry, &b);
+		}
+		else if (count == 2)
+		{
+			EXPECT_EQ(&entry, &c);
+		}
+
+		++count;
+	}
+	EXPECT_EQ(count, 3);
+
+	count = 0;
+	for (SimpleListEntryType& entry : listA)
+	{
+		++count;
+	}
+	EXPECT_EQ(count, 0);
+}
+
+TEST(IntrusveList, MoveAssignment)
+{
+	SimpleListEntryType a {'a'};
+	SimpleListEntryType b {'b'};
+	SimpleListEntryType c {'c'};
+	SimpleListEntryType d {'d'};
+
+	SimpleHeartIntrusiveList listA;
+	listA.PushBack(&a);
+	listA.PushBack(&b);
+	listA.PushBack(&c);
+
+	EXPECT_EQ(listA.Size(), 3);
+
+	SimpleHeartIntrusiveList listB;
+	listB.PushBack(&d);
+
+	EXPECT_EQ(listB.Size(), 1);
+	EXPECT_TRUE(listA.Contains(&a));
+	EXPECT_TRUE(listB.Contains(&d));
+
+	listB = std::move(listA);
+
+	EXPECT_EQ(listA.Size(), 0);
+	EXPECT_EQ(listB.Size(), 3);
+	EXPECT_FALSE(listA.Contains(&a));
+	EXPECT_FALSE(listA.Contains(&b));
+	EXPECT_FALSE(listA.Contains(&c));
+	EXPECT_TRUE(listB.Contains(&a));
+	EXPECT_TRUE(listB.Contains(&b));
+	EXPECT_TRUE(listB.Contains(&c));
+
+	int count = 0;
+	for (SimpleListEntryType& entry : listB)
+	{
+		if (count == 0)
+		{
+			EXPECT_EQ(&entry, &a);
+		}
+		else if (count == 1)
+		{
+			EXPECT_EQ(&entry, &b);
+		}
+		else if (count == 2)
+		{
+			EXPECT_EQ(&entry, &c);
+		}
+
+		++count;
+	}
+	EXPECT_EQ(count, 3);
+
+	count = 0;
+	for (SimpleListEntryType& entry : listA)
+	{
+		++count;
+	}
+	EXPECT_EQ(count, 0);
+}
