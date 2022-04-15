c3ade7ccd4a2c161bf4a292a89e14ed9a13bbf18
James Keats <growlitheharpo@gmail.com>
2022-04-15 17:36:35 -0400

    Add modified BSD-3-Clause license

--- game/src/ui/ui_manager.cpp
+++ game/src/ui/ui_manager.cpp
@@ -1,3 +1,12 @@
+/* Copyright (C) 2022 James Keats
+*
+* You may use, distribute, and modify this code under the terms of its modified
+* BSD-3-Clause license. Use for any commercial purposes is prohibited.
+
+* You should have received a copy of the license with this file. If not, please visit:
+* https://github.com/growlitheharpo/heart-engine-playground
+*
+*/
 #include "ui_manager.h"
 
 #include "ui/button.h"
