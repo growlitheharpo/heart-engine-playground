beb5bd87a752e070d6d7a643cecc146c2af9cdc7
James Keats <growlitheharpo@gmail.com>
2022-08-09 16:45:09 -0400

    Add non _t typedefs and other string_view types to hash

--- heart/heart-core/include/heart/types.h
+++ heart/heart-core/include/heart/types.h
@@ -18,3 +18,13 @@ typedef uint8_t byte_t;
 static constexpr size_t Kilo = 0x400ull;
 static constexpr size_t Meg = Kilo * Kilo;
 static constexpr size_t Gig = Meg * Kilo;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
